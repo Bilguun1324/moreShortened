18da80585a4779ae3742c752682d00130b23f654
Bilguun <bilguun@Bilguuns-MacBook-Pro.local>
2020-08-07 16:45:42 +0800

    ugutbg

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+// import { HomeDefault, Login, Tuuh, Signup } from './pages';
+// import { Shalgagch } from './pages/shalgagch';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+// import './style/main.scss';
+// import { AuthUserProvider } from './provider/auth-user-provider';
+// import { ShortUrlProvider } from './provider/short-url-provider';
 
-function App() {
-  return (<>
-  </>);
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <HomeDefault />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;

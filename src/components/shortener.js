08620368279a28cbe034cb5074588e04a63e15b1
Bilguun <bilguun@Bilguuns-MacBook-Pro.local>
2020-08-07 17:32:22 +0800

    something

--- src/components/shortener.js
+++ src/components/shortener.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useInput, useKey } from '../hooks/firebase';
 
 export const HomeDefault = () => {
+
+    const [urlholder, bindurlholder] = useInput('');
+
     return (
-    <div>
-        <div>Shorten your URL</div>
-        <input></input>
-    </div>
+        <div>
+            <div>Shorten your URL</div>
+            <div className='urlcont'>
+                <input {...bindurlholder} className='input' placeholder='Your URL Here'></input>
+                <button className='btn'>Shorten</button>
+            </div>
+        </div>
     )
 }
